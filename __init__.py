85c5a441addc2c0ec326535d4decb8daf6c2eccc
Rahul Bansal <Rahul.Bansal.cs116@cse.iitd.ac.in>
2018-10-08 10:07:03 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,12 @@
+import requests
+import wikipedia
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
